13910a9a5dd5f75734278f369c96c7940df1e78e
Tata Coa <tatacoa@Tatas-MacBook-Pro.local>
2018-02-07 20:20:21 +0100

    added DSL language

---
+++ dsl.py
@@ -0,0 +1,16 @@
+
+PROGRAM = '>>>>>p+p++p+++p<<<<<+++p>p+p+++++>+p<<-p'
+
+memory = [0 for i in range(10)]
+index = 0
+for cmd in PROGRAM:
+    if cmd == '+':
+        memory[index] += 1
+    elif cmd == '-':
+        memory[index] -= 1
+    if cmd == '>':
+        index += 1
+    elif cmd == '>':
+        index -= 1
+    elif cmd == 'p':
+        print(memory[index])
